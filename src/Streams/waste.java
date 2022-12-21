fa35b334ba371e90952196e65b13687cf57d7ec9
Pathak1097 <pathak.r.prashant10@gmail.com>
2022-12-21 11:03:45 +0530

    devChanged

--- src/Streams/waste.java
+++ src/Streams/waste.java
@@ -4,6 +4,7 @@ public class waste {
 	public static void main(String[] args) {
 		System.out.println("Wasted File");
 		System.out.println("Add Feature1");
+		System.out.println("Dev branch");
 	}
 
 }
